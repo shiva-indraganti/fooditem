45bf22b34fa9f2859399a94c43ae1c5a86652ff7
shiva-indraganti <issksharma2020@gmail.com>
2025-01-05 22:11:29 +0530

    food item

---
+++ src/main/java/com/example/fooditemservice/FooditemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fooditemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooditemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooditemserviceApplication.class, args);
+	}
+
+}
